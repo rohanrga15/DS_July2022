0ec9c2945f3aeb9f633dceab7883b8f2cceffa08
rohanrga15 <109783725+rohanrga15@users.noreply.github.com>
2022-08-02 18:52:01 +0530

    Add files via upload

---
+++ JDBC/AssignmentSelectAllJDBC.java
@@ -0,0 +1,59 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AssignmentSelectAllJDBC 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Registering driver");
+		try 
+		{
+			DriverManager.registerDriver(new org.hsqldb.jdbc.JDBCDriver());
+			System.out.println("Registered");
+			System.out.println("Trying to connect");
+			Connection conn = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/xdb", "SA", "");
+			System.out.println("Connected to "+conn);
+			System.out.println("Trying to make a statement");
+			Statement state = conn.createStatement();
+			System.out.println("Statement created"+state);
+			System.out.println("Trying to execute statement");
+			ResultSet rs = state.executeQuery("SELECT * FROM STUDENTSSS");
+			System.out.println("Statement executed");
+			
+			while(rs.next())
+			{
+				int rollno = rs.getInt(1);
+				String stname = rs.getString(2);
+				int stbdate = rs.getInt(3);
+				int stphy = rs.getInt(4);
+				int stchem = rs.getInt(6);
+				int stmaths = rs.getInt(7);
+				int total = rs.getInt(6);
+				int stgrade = rs.getInt(7);
+				System.out.println("------------------------");
+				System.out.println("Employee number : "+rollno);
+				System.out.println("Employee name 	: "+stname);
+				System.out.println("Employee salary : "+stbdate);
+				System.out.println("Employee number : "+stphy);
+				System.out.println("Employee name 	: "+stchem);
+				System.out.println("Employee salary : "+stmaths);
+				System.out.println("Employee number : "+total);
+				System.out.println("Employee name 	: "+stgrade);
+				System.out.println("------------------------");
+			}
+			rs.close();
+			state.close();
+			conn.close();
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		
+	}
+}
+
